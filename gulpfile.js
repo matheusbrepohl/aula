199a65c45667d9a893232e5b0102713d5d145140
matheusbrepohl <matheus@brepohl.net>
2020-12-03 15:23:36 +0100

    2020-12-03-15-23

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
-var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-var watch = require('gulp-watch');
+//antigo
 
+//var gulp = require('gulp');
+//var sass = require('gulp-ruby-sass');
+//var watch = require('gulp-watch');
 
 //task para o sass
-gulp.task('sass', function(){
-    return sass('sass/**/*.sass').pipe(gulp.dest('css'));
-});
+//gulp.task('sass', function() {
+//    return sass('sass/**/*.sass').pipe(gulp.dest('css'));
+//});
+
+const gulp =require('gulp');
+const sass =require('gulp-sass');
+const browserSync =require('browser-sync').create();
+
+function style() {
+    return gulp.src('./sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./sass/**/*.sass', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+
