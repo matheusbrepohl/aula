19d10b75945241db65af0169381905bd7bcd13e9
matheusbrepohl <matheus@brepohl.net>
2020-12-03 12:16:25 +0100

    gulp last

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var watch = require('gulp-watch');
 
 
 //task para o sass
-gulp.task('sass', function(){
+gulp.task('default', function(){
     return sass('sass/**/*.sass').pipe(gulp.dest('css'));
 });
